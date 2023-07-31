1ceb8f2e54b7f8cd4956e896c426d9d5a808d4b1
LustaY <lusta.yu.a@gmail.com>
2023-08-01 00:10:19 +0300

    Second part

--- src/ArtGallery.API/Startup.cs
+++ src/ArtGallery.API/Startup.cs
@@ -1,4 +1,4 @@
-﻿using ArtGallery.Infrastructure;
+﻿using ArtGallery.Infrastructure.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 
@@ -18,7 +18,34 @@ namespace ArtGallery.API
             {
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
             });
+
             services.AddControllers();
+
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+            });
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
         }
     }
 }
